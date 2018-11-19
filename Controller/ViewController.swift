c14a7e617165f2135bf518076c0f04c6bcb2916c
Maxime6 <maxime.tanter@hotmail.fr>
2018-11-19 13:43:54 +0100

    Set up responsive design for iphone in portrait mode

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var stringNumbers: [String] = [String()]
     var operators: [String] = ["+"]
     var index = 0
+    
     var isExpressionCorrect: Bool {
         if let stringNumber = stringNumbers.last {
             if stringNumber.isEmpty {
@@ -130,4 +131,5 @@ class ViewController: UIViewController {
         operators = ["+"]
         index = 0
     }
+    
 }
---
+++ Model/Calculator.swift
@@ -0,0 +1,25 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Yves TANTER on 19/11/2018.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class Calculator {
+    
+//    var stringNumbers: [String] = [String()]
+//    var operators: [String] = ["+"]
+//    var index = 0
+//    
+//    func clear() {
+//        stringNumbers = [String()]
+//        operators = ["+"]
+//        index = 0
+//    }
+    
+}
